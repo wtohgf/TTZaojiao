bdd211df2570a1f1219065f67b49f6f563a3b64d
Heguofeng <xxx@qq.com>
2015-04-17 10:12:10 +0800

    添加 登录注册页面

---
+++ TTzaojiao/TTzaojiao/TTLogRegViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTLogRegViewController.h
+//  TTzaojiao
+//
+//  Created by hegf on 15-4-17.
+//  Copyright (c) 2015年 hegf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTLogRegViewController : UIViewController
+
+@end
