1aa18a6af63a1ac12636e968530581680eca8c7e
Liang Zhang <zhangarthur@zhangliang.local>
2015-04-23 17:00:53 +0800

    个人信息 Page

--- TTzaojiao/TTzaojiao/TTLaMaViewController.m
+++ TTzaojiao/TTzaojiao/TTLaMaViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "TTLaMaViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface TTLaMaViewController ()
-
+@interface TTLaMaViewController () <CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager* locationManager;
 @end
 
 @implementation TTLaMaViewController
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [self startLocation];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,4 +45,55 @@
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 5;
 }
+
+
+
+//开始定位
+
+-(void)startLocation{
+    
+    if ([CLLocationManager locationServicesEnabled])
+    {
+        if (!self.locationManager)
+        {
+            self.locationManager = [[CLLocationManager alloc] init];
+        }
+        self.locationManager.delegate = self;
+        self.locationManager.distanceFilter=1.0;
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        
+        if([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)])
+        {
+            [self.locationManager requestAlwaysAuthorization]; // 永久授权
+            [self.locationManager requestWhenInUseAuthorization]; //使用中授权
+        }
+        
+        [self.locationManager startUpdatingLocation];//开启位置更新
+    }
+}
+
+
+//定位代理经纬度回调
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    [_locationManager stopUpdatingLocation];
+    
+    CLLocation *newLocation = [locations lastObject];
+    NSLog(@"%@",[NSString stringWithFormat:@"经度:%3.5f\n纬度:%3.5f",newLocation.coordinate.latitude,newLocation.coordinate.longitude]);
+    
+    CLGeocoder * geoCoder = [[CLGeocoder alloc] init];
+    
+    [geoCoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        
+        for (CLPlacemark * placemark in placemarks) {
+            
+            NSDictionary *test = [placemark addressDictionary];
+    
+            //  Country(国家)  State(城市)  SubLocality(区)
+            
+            NSLog(@"\n国家:%@\n省:%@\n城市:%@\n街道:%@", [test objectForKey:@"Country"],[test objectForKey:@"State"],[test objectForKey:@"City"],[test objectForKey:@"Street"]);
+            
+        }
+    }];
+}
+
 @end
