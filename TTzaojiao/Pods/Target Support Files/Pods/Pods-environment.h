05e0cf3c57d66762ab076a81f8852552bbc7bc6f
Liang Zhang <zhangarthur@zhangliang.local>
2015-04-29 15:55:51 +0800

    pod setting

--- TTzaojiao/Pods/Target Support Files/Pods/Pods-environment.h
+++ TTzaojiao/Pods/Target Support Files/Pods/Pods-environment.h
@@ -102,15 +102,9 @@
 #define COCOAPODS_VERSION_MINOR_SKSplashView 1
 #define COCOAPODS_VERSION_PATCH_SKSplashView 0
 
-// SlackTextViewController
-#define COCOAPODS_POD_AVAILABLE_SlackTextViewController
-#define COCOAPODS_VERSION_MAJOR_SlackTextViewController 1
-#define COCOAPODS_VERSION_MINOR_SlackTextViewController 5
-#define COCOAPODS_VERSION_PATCH_SlackTextViewController 0
-
-// SlackTextViewController/Additions
-#define COCOAPODS_POD_AVAILABLE_SlackTextViewController_Additions
-#define COCOAPODS_VERSION_MAJOR_SlackTextViewController_Additions 1
-#define COCOAPODS_VERSION_MINOR_SlackTextViewController_Additions 5
-#define COCOAPODS_VERSION_PATCH_SlackTextViewController_Additions 0
+// ZWIntroductionViewController
+#define COCOAPODS_POD_AVAILABLE_ZWIntroductionViewController
+#define COCOAPODS_VERSION_MAJOR_ZWIntroductionViewController 0
+#define COCOAPODS_VERSION_MINOR_ZWIntroductionViewController 2
+#define COCOAPODS_VERSION_PATCH_ZWIntroductionViewController 1
 
